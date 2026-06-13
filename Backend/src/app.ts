488e768d908fab890b1107b46ed73da9729098b7
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-06-13 12:39:28 +0700

    Setup the endpoints of authentication,user,patient & root admin initialization

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,27 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
+import patientRoute from "./routes/patientRoute.js";
+import { notFoundHandler } from "./middlewares/notFoundMiddleware.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/patients', patientRoute);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
 
 export default app;
