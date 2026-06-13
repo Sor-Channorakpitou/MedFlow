75889bc28dd3e24e222ac39b9e429ffdb0cb5b6e
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-06-13 12:29:20 +0700

    Adding fields to User & Appointment

---
+++ Backend/prisma/migrations/20260608084759_database_schema/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "dateOfBirth" SET DATA TYPE DATE;
+
+-- CreateTable
+CREATE TABLE "playing_with_neon" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" REAL,
+
+    CONSTRAINT "playing_with_neon_pkey" PRIMARY KEY ("id")
+);
