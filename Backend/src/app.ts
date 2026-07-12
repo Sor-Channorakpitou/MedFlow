a92589e367c1dd1d32cc375bbdc7e158c5e74478
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-07-12 14:57:36 +0700

    aff patch to cors allow

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -26,7 +26,7 @@ dotenv.configDotenv();
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     optionsSuccessStatus: 200,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
