b8e21e1d014cdb6a2d2dd90ae8e9bf6089f9ccea
SorChannorakpitou <channorakpitou.sor@student.cadt.edu.kh>
2026-07-12 14:42:41 +0700

    add transport to socket

--- Frontend/medflow-web/src/services/socket.js
+++ Frontend/medflow-web/src/services/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
     autoConnect: false,
     withCredentials: true,
+    transports: ["websocket"],
 });
 
 export function connectSocket(accessToken) {
